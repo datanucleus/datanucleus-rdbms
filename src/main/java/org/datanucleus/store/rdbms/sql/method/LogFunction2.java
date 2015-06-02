c06a2f97df3395adbeff832bf3fe6989af6063a4
andyjefferson <andy@datanucleus.org>
2015-06-02 09:43:30 +0100

    [NUCRDBMS-897] Process log on Postgresql using "LN"

---
+++ src/main/java/org/datanucleus/store/rdbms/sql/method/LogFunction2.java
@@ -0,0 +1,40 @@
+/**********************************************************************
+Copyright (c) 2008 Andy Jefferson and others. All rights reserved.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+Contributors:
+    ...
+**********************************************************************/
+package org.datanucleus.store.rdbms.sql.method;
+
+/**
+ * Expression handler to invoke the SQL (natural) LOG function.
+ * For use in evaluating LN({expr}) where the RDBMS supports this function.
+ * Returns a NumericExpression "LN({numericExpr})".
+ */
+public class LogFunction2 extends SimpleNumericMethod
+{
+    protected String getFunctionName()
+    {
+        return "LN";
+    }
+
+    /* (non-Javadoc)
+     * @see org.datanucleus.store.rdbms.sql.method.SimpleNumericMethod#getClassForMapping()
+     */
+    @Override
+    protected Class getClassForMapping()
+    {
+        return double.class;
+    }
+}
