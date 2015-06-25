4580eca5c1227020dbb72a0f059194c32a83c2f3
andyjefferson <andy@datanucleus.org>
2015-06-25 12:25:39 +0100

    [NUCRDBMS-904] Remove read uncommitted

--- src/main/java/org/datanucleus/store/rdbms/adapter/PostgreSQLAdapter.java
+++ src/main/java/org/datanucleus/store/rdbms/adapter/PostgreSQLAdapter.java
@@ -103,6 +103,7 @@ public class PostgreSQLAdapter extends BaseDatastoreAdapter
         supportedOptions.add(STORED_PROCEDURES);
         supportedOptions.remove(TX_ISOLATION_NONE);
         supportedOptions.remove(ALLOW_TABLE_ALIAS_IN_UPDATE_SET_CLAUSE);
+        supportedOptions.remove(TX_ISOLATION_READ_UNCOMMITTED); // Not supported in PostgreSQL AFAIK
     }
 
     /**
