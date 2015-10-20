27369940193a6e9791e4b95276675f8b680c4b39
andyjefferson <andy@datanucleus.org>
2015-10-20 08:19:44 +0100

    placeholder

---
+++ src/main/java/org/datanucleus/store/rdbms/sql/InsertStatement.java
@@ -0,0 +1,59 @@
+/**********************************************************************
+Copyright (c) 2015 Andy Jefferson and others. All rights reserved.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+Contributors:
+     ...
+ **********************************************************************/
+package org.datanucleus.store.rdbms.sql;
+
+import java.util.Map;
+
+import org.datanucleus.store.rdbms.RDBMSStoreManager;
+import org.datanucleus.store.rdbms.identifier.DatastoreIdentifier;
+import org.datanucleus.store.rdbms.table.Table;
+
+/**
+ * SQL INSERT Statement representation.
+ * This will create a statement like
+ * <pre>
+ * INSERT INTO {tbl} (col1, col2, ...)
+ * SELECT ...
+ * </pre>
+ */
+public class InsertStatement extends SQLStatement
+{
+    /**
+     * Constructor for an UPDATE statement.
+     * @param rdbmsMgr Store Manager
+     * @param table The primary table to UPDATE
+     * @param alias Alias for the primary table
+     * @param tableGroupName Group name for the primary table
+     * @param extensions Any extensions (optional)
+     */
+    public InsertStatement(RDBMSStoreManager rdbmsMgr, Table table, DatastoreIdentifier alias, String tableGroupName, Map<String, Object> extensions)
+    {
+        super(null, rdbmsMgr, table, alias, tableGroupName, extensions);
+    }
+
+    public synchronized SQLText getSQLText()
+    {
+        if (sql != null)
+        {
+            return sql;
+        }
+
+        // TODO Generate the statement
+        return sql;
+    }
+}
