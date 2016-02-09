656e9a13dd6cb47e197e335659f04e63f6d2c15d
andyjefferson <andy@datanucleus.org>
2016-02-09 12:07:48 +0000

    [NUCRDBMS-1003] Add support for Optional.get/isPresent

---
+++ src/main/java/org/datanucleus/store/rdbms/sql/expression/OptionalExpression.java
@@ -0,0 +1,44 @@
+/**********************************************************************
+Copyright (c) 2016 Andy Jefferson and others. All rights reserved.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+Contributors:
+     ...
+ **********************************************************************/
+package org.datanucleus.store.rdbms.sql.expression;
+
+import java.util.List;
+
+import org.datanucleus.store.rdbms.mapping.java.JavaTypeMapping;
+import org.datanucleus.store.rdbms.sql.SQLStatement;
+import org.datanucleus.store.rdbms.sql.SQLTable;
+
+/**
+ * Expression for java.util.Optional.
+ */
+public class OptionalExpression extends SingleCollectionExpression
+{
+    public OptionalExpression(SQLStatement stmt, SQLTable table, JavaTypeMapping mapping)
+    {
+        super(stmt, table, mapping);
+    }
+
+    /* (non-Javadoc)
+     * @see org.datanucleus.store.rdbms.sql.expression.DelegatedExpression#invoke(java.lang.String, java.util.List)
+     */
+    @Override
+    public SQLExpression invoke(String methodName, List args)
+    {
+        return stmt.getRDBMSManager().getSQLExpressionFactory().invokeMethod(stmt, java.util.Optional.class.getName(), methodName, this, args);
+    }
+}
---
+++ src/main/java/org/datanucleus/store/rdbms/sql/method/OptionalGetMethod.java
@@ -0,0 +1,46 @@
+/**********************************************************************
+Copyright (c) 2016 Andy Jefferson and others. All rights reserved.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+Contributors:
+    ...
+**********************************************************************/
+package org.datanucleus.store.rdbms.sql.method;
+
+import java.util.List;
+
+import org.datanucleus.exceptions.NucleusException;
+import org.datanucleus.store.rdbms.mapping.java.OptionalMapping;
+import org.datanucleus.store.rdbms.sql.expression.OptionalExpression;
+import org.datanucleus.store.rdbms.sql.expression.SQLExpression;
+
+/**
+ * Method for evaluating {optionalExpr1}.get().
+ * Returns a XXXExpression representing the stored object.
+ */
+public class OptionalGetMethod extends AbstractSQLMethod
+{
+    /* (non-Javadoc)
+     * @see org.datanucleus.store.rdbms.sql.method.SQLMethod#getExpression(org.datanucleus.store.rdbms.sql.expression.SQLExpression, java.util.List)
+     */
+    public SQLExpression getExpression(SQLExpression expr, List<SQLExpression> args)
+    {
+        if (args != null && args.size() > 0)
+        {
+            throw new NucleusException("Optional.get should be passed no arguments");
+        }
+
+        OptionalMapping opMapping = (OptionalMapping) ((OptionalExpression)expr).getJavaTypeMapping();
+        return exprFactory.newExpression(stmt, expr.getSQLTable(), opMapping.getWrappedMapping());
+    }
+}
