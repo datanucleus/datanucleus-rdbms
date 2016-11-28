05ae6d7c334011a345b0edb7ae5fbeacb28a2e46
andyjefferson <andy@datanucleus.org>
2016-11-28 14:14:20 +0000

    Support SQLServer String.length using LEN. Fixes #146

---
+++ src/main/java/org/datanucleus/store/rdbms/sql/method/StringLength4Method.java
@@ -0,0 +1,61 @@
+/**********************************************************************
+Copyright (c) 2016 Andy Jefferson and others. All rights reserved.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+Contributors:
+    ...
+**********************************************************************/
+package org.datanucleus.store.rdbms.sql.method;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.datanucleus.exceptions.NucleusException;
+import org.datanucleus.store.rdbms.mapping.java.JavaTypeMapping;
+import org.datanucleus.store.rdbms.sql.expression.IntegerLiteral;
+import org.datanucleus.store.rdbms.sql.expression.NumericExpression;
+import org.datanucleus.store.rdbms.sql.expression.ParameterLiteral;
+import org.datanucleus.store.rdbms.sql.expression.SQLExpression;
+import org.datanucleus.store.rdbms.sql.expression.StringExpression;
+import org.datanucleus.store.rdbms.sql.expression.StringLiteral;
+import org.datanucleus.util.Localiser;
+
+/**
+ * Expression handler to evaluate {stringExpression}.length().
+ * Returns a NumericExpression <pre>LEN({stringExpr})</pre>.
+ */
+public class StringLength4Method extends AbstractSQLMethod
+{
+    /* (non-Javadoc)
+     * @see org.datanucleus.store.rdbms.sql.method.SQLMethod#getExpression(org.datanucleus.store.rdbms.sql.expression.SQLExpression, java.util.List)
+     */
+    public SQLExpression getExpression(SQLExpression expr, List<SQLExpression> args)
+    {
+        if (!expr.isParameter() && expr instanceof StringLiteral)
+        {
+            JavaTypeMapping m = exprFactory.getMappingForType(int.class, false);
+            String val = (String)((StringLiteral)expr).getValue();
+            return new IntegerLiteral(stmt, m, Integer.valueOf(val.length()), null);
+        }
+        else if (expr instanceof StringExpression || expr instanceof ParameterLiteral)
+        {
+            ArrayList funcArgs = new ArrayList();
+            funcArgs.add(expr);
+            return new NumericExpression(stmt, getMappingForClass(int.class), "LEN", funcArgs);
+        }
+        else
+        {
+            throw new NucleusException(Localiser.msg("060001", "length", expr));
+        }
+    }
+}
