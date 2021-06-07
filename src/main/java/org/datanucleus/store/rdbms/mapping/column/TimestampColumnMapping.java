14c237b2aac26abfcb06bbf0aa9cb10508756ecd
andyjefferson <andy@datanucleus.org>
2021-06-07 14:33:55 +0100

    Move conversion methods to this class since only used here

--- src/main/java/org/datanucleus/store/rdbms/mapping/column/TimestampColumnMapping.java
+++ src/main/java/org/datanucleus/store/rdbms/mapping/column/TimestampColumnMapping.java
@@ -26,8 +26,11 @@ import java.sql.SQLException;
 import java.sql.Time;
 import java.sql.Timestamp;
 import java.sql.Types;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Iterator;
 
 import org.datanucleus.ClassNameConstants;
 import org.datanucleus.exceptions.NucleusDataStoreException;
@@ -35,7 +38,6 @@ import org.datanucleus.store.rdbms.RDBMSStoreManager;
 import org.datanucleus.store.rdbms.mapping.java.JavaTypeMapping;
 import org.datanucleus.store.rdbms.table.Column;
 import org.datanucleus.util.Localiser;
-import org.datanucleus.util.TypeConversionHelper;
 
 /**
  * Mapping of a TIMESTAMP column.
@@ -182,7 +184,7 @@ public class TimestampColumnMapping extends AbstractColumnMapping
                 else
                 {
                     // JDBC driver has returned something other than a java.sql.Timestamp so we convert it to a Timestamp using its string form.
-                    value = s == null ? null : TypeConversionHelper.stringToTimestamp(s, cal);
+                    value = s == null ? null : stringToTimestamp(s, cal);
                 }
             }
             catch (SQLException nestedEx)
@@ -224,5 +226,117 @@ public class TimestampColumnMapping extends AbstractColumnMapping
         {
             return value;
         }
+    }
+
+    /**
+     * Converts a string in JDBC timestamp escape format to a Timestamp object.
+     * To be precise, we prefer to find a JDBC escape type sequence in the format "yyyy-mm-dd hh:mm:ss.fffffffff", but this does not accept
+     * other separators of fields, so as long as the numbers are in the order year, month, day, hour, minute, second then we accept it.
+     * @param s Timestamp string
+     * @param cal The Calendar to use for conversion
+     * @return Corresponding <i>java.sql.Timestamp</i> value.
+     * @exception java.lang.IllegalArgumentException Thrown if the format of the
+     * String is invalid
+     */
+    private static Timestamp stringToTimestamp(String s, Calendar cal)
+    {
+        int[] numbers = convertStringToIntArray(s);
+        if (numbers == null || numbers.length < 6)
+        {
+            throw new IllegalArgumentException(Localiser.msg("030003", s));
+        }
+
+        int year = numbers[0];
+        int month = numbers[1];
+        int day = numbers[2];
+        int hour = numbers[3];
+        int minute = numbers[4];
+        int second = numbers[5];
+        int nanos = 0;
+        if (numbers.length > 6)
+        {
+            StringBuilder zeroedNanos = new StringBuilder("" + numbers[6]);
+            if (zeroedNanos.length() < 9)
+            {
+                // Add trailing zeros
+                int numZerosToAdd = 9-zeroedNanos.length();
+                for (int i=0;i<numZerosToAdd;i++)
+                {
+                    zeroedNanos.append("0");
+                }
+                nanos = Integer.valueOf(zeroedNanos.toString());
+            }
+            else
+            {
+                nanos = numbers[6];
+            }
+        }
+
+        Calendar thecal = cal;
+        if (cal == null)
+        {
+            thecal = new GregorianCalendar();
+        }
+        thecal.set(Calendar.ERA, GregorianCalendar.AD);
+        thecal.set(Calendar.YEAR, year);
+        thecal.set(Calendar.MONTH, month - 1);
+        thecal.set(Calendar.DATE, day);
+        thecal.set(Calendar.HOUR_OF_DAY, hour);
+        thecal.set(Calendar.MINUTE, minute);
+        thecal.set(Calendar.SECOND, second);
+        Timestamp ts = new Timestamp(thecal.getTime().getTime());
+        ts.setNanos(nanos);
+
+        return ts;
+    }
+
+    /**
+     * Convenience method to convert a String containing numbers (separated by assorted
+     * characters) into an int array. The separators can be ' '  '-'  ':'  '.'  ',' etc.
+     * @param str The String
+     * @return The int array
+     */
+    private static int[] convertStringToIntArray(String str)
+    {
+        if (str == null)
+        {
+            return null;
+        }
+
+        int[] values = null;
+        ArrayList list = new ArrayList();
+
+        int start = -1;
+        for (int i=0;i<str.length();i++)
+        {
+            if (start == -1 && Character.isDigit(str.charAt(i)))
+            {
+                start = i;
+            }
+            if (start != i && start >= 0)
+            {
+                if (!Character.isDigit(str.charAt(i)))
+                {
+                    list.add(Integer.valueOf(str.substring(start, i)));
+                    start = -1;
+                }
+            }
+            if (i == str.length()-1 && start >= 0)
+            {
+                list.add(Integer.valueOf(str.substring(start)));
+            }
+        }
+
+        if (!list.isEmpty())
+        {
+            values = new int[list.size()];
+            Iterator iter = list.iterator();
+            int n = 0;
+            while (iter.hasNext())
+            {
+                values[n++] = ((Integer)iter.next()).intValue();
+            }
+        }
+        return values;
     }
 }
