be1700fd0b1f394aad7109339bf29c931189cdab
andyjefferson <andy@datanucleus.org>
2021-09-26 10:55:39 +0100

    Rename "datanucleus.useIsNullWhenEqualsNullParameter" query extension. Fixes #404

--- src/main/java/org/datanucleus/store/rdbms/query/JPQLQuery.java
+++ src/main/java/org/datanucleus/store/rdbms/query/JPQLQuery.java
@@ -103,22 +103,22 @@ public class JPQLQuery extends AbstractJPQLQuery
     private static final long serialVersionUID = -3735379324740714088L;
 
     /** Extension for whether to convert "== ?" with null parameter to "IS NULL". Defaults to false to comply with JPA spec 4.11. */
-    public static final String EXTENSION_USE_IS_NULL_WHEN_EQUALS_NULL_PARAM = "datanucleus.useIsNullWhenEqualsNullParameter";
+    public static final String EXTENSION_USE_IS_NULL_WHEN_EQUALS_NULL_PARAM = "datanucleus.query.useIsNullWhenEqualsNullParameter";
 
     /** Extension to add NOWAIT when using FOR UPDATE (when supported). */
     public static final String EXTENSION_FOR_UPDATE_NOWAIT = "datanucleus.query.forUpdateNowait";
-
-    /** Extension to define the JOIN TYPE to use when navigating single-valued relations, when part of the filter. */
-    public static final String EXTENSION_NAVIGATION_JOIN_TYPE_FILTER = "datanucleus.query.jpql.navigationJoinTypeForFilter";
-
-    /** Extension to define the JOIN TYPE to use when navigating single-valued relations. */
-    public static final String EXTENSION_NAVIGATION_JOIN_TYPE = "datanucleus.query.jpql.navigationJoinType";
 
     /** Extension to not apply a discriminator restriction on the candidate of the query. */
     public static final String EXTENSION_CANDIDATE_DONT_RESTRICT_DISCRIMINATOR = "datanucleus.query.dontRestrictDiscriminator";
 
     /** Extension to include soft-deleted objects in any results. */
     public static final String EXTENSION_INCLUDE_SOFT_DELETES = "datanucleus.query.includeSoftDeletes";
+
+    /** Extension to define the JOIN TYPE to use when navigating single-valued relations, when part of the filter. */
+    public static final String EXTENSION_NAVIGATION_JOIN_TYPE_FILTER = "datanucleus.query.jpql.navigationJoinTypeForFilter";
+
+    /** Extension to define the JOIN TYPE to use when navigating single-valued relations. */
+    public static final String EXTENSION_NAVIGATION_JOIN_TYPE = "datanucleus.query.jpql.navigationJoinType";
 
     /** The compilation of the query for this datastore. Not applicable if totally in-memory. */
     protected transient RDBMSQueryCompilation datastoreCompilation;
