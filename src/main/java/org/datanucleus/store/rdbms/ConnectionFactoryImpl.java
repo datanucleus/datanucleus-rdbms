1106e43f12d6b3d68c1a09f847398efe12b3623c
andyjefferson <andy@datanucleus.org>
2025-04-06 14:57:20 +0100

    Add handling for MC when an exception is thrown so that the super class handles it before we throw the required exception. Fixes #501

--- src/main/java/org/datanucleus/store/rdbms/ConnectionFactoryImpl.java
+++ src/main/java/org/datanucleus/store/rdbms/ConnectionFactoryImpl.java
@@ -364,6 +364,10 @@ public class ConnectionFactoryImpl extends AbstractConnectionFactory
                 }
                 catch (SQLException sqle)
                 {
+                    NucleusLogger.GENERAL.error(">> MC.release threw exception ", sqle);
+                    // Process the release before we throw the exception
+                    super.release();
+
                     throw new NucleusDataStoreException(sqle.getMessage(), sqle);
                 }
             }
